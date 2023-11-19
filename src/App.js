3c4f5b70e1ac5cff36c8f161ec8833584290b7ee
github_federicolss <thiagoVernanchet.2022@gmail.com>
2023-11-19 16:30:43 -0300

    modified:   .gitignore 	modified:   src/App.js 	new file:   src/component/About.jsx 	new file:   src/component/Contact.jsx 	new file:   src/component/Footer.jsx 	new file:   src/component/Header.jsx 	new file:   src/component/Hobbies.jsx 	new file:   src/component/Home.jsx 	new file:   src/component/NotFound.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
+import Home from './component/Home';
+import About from './component/About';
+import Hobbies from './component/Hobbies';
+import Contact from './component/Contact';
+import Footer from './component/Footer';
+import NotFound from './component/NotFound';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/hobbies" element={<Hobbies />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
